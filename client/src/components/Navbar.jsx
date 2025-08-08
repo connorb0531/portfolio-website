dc9fb122ba75f8bb90e2419a5c8cb65ad0fda29e
Connor Buckley <connorbuckley144@gmail.com>
2025-08-07 18:31:09 -0700

    Update Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 export default function Navbar() {
   return (
-    <nav className="bg-navbar-grey text-white p-5 flex justify-center space-x-6 shadow-xl">
-      <a href="#home" className="hover:underline">Home</a>
-      <a href="#about" className="hover:underline">About</a>
-      <a href="#projects" className="hover:underline">Projects</a>
-      <a href="#contact" className="hover:underline">Contact</a>
+    <nav className="bg-navbar-grey text-white text-md font-bold p-5 flex justify-center space-x-6 shadow-xl">
+      <a href="#home" className="hover:text-slate-500 transition">Home</a>
+      <a href="#about" className="hover:text-slate-500 transition">About</a>
+      <a href="#projects" className="hover:text-slate-500 transition">Projects</a>
+      <a href="#contact" className="hover:text-slate-500 transition0">Contact</a>
     </nav>
   );
 }
