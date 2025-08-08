c9a6d4835ecedcd4269211105d242bd00274b327
Connor Buckley <connorbuckley144@gmail.com>
2025-08-07 20:19:10 -0700

    Update Projects Page

---
+++ client/src/data/projects.js
@@ -0,0 +1,25 @@
+export const projects = [
+    {
+      title: "NASA DE440 Simulator",
+      blurb:
+        "Solar system simulator inspired by NASA's JPL DE440 ephemerides. Built from scratch in Python using real-world data, Newtonian physics, and numerical integrators.",
+      images: ["/projects/DE440-simulation2.png"],
+      link: "https://github.com/connorb0531/DE440-Simulator",
+    },
+    {
+      title: "Construction Website",
+      blurb:
+        "Full-stack site with MongoDB, Express, React, Node. Deployed on Render.",
+      images: ["/projects/construction-website1.png", "/projects/construction-website2.png"],
+      link: "https://tbuckdev.com",
+    },
+    {
+        title: "FitBit Conntected Lock",
+        blurb:
+          "Solenoid lock using Fitbit API and Raspberry Pi to unlock a door when activity goals (steps, distance, calories) are achieved.",
+        images: ["/projects/fitbit-lock1.jpeg"],
+        link: "https://github.com/connorb0531/FitBit-Lock",
+      },
+    
+  ];
+  
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,19 @@
-export default function Proejcts() {
-    return (
-        <div>
-            Projects
-        </div>
-    );
+import { projects } from "../data/projects";
+import ProjectSection from "../components/ProjectSection";
+
+export default function Projects() {
+  return (
+    <div id="projects" className="min-h-screen px-2 py-12 max-w-screen-lg mx-auto">
+      {/* Page Title */}
+      <h2 className="text-4xl font-bold text-center">
+        Projects
+      </h2>
+
+      <main className="snap-y snap-mandatory">
+        {projects.map((p, i) => (
+          <ProjectSection key={p.title} {...p} reverse={i % 2 === 1} />
+        ))}
+      </main>
+    </div>
+  );
 }
