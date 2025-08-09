bafa07f737a6c56672c3b6e399afd5afeb5f1706
Connor Buckley <connorbuckley144@gmail.com>
2025-08-08 20:50:41 -0700

    Increase Navbar shadow

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="bg-navbar-grey text-white text-md font-bold p-5 flex justify-center space-x-6 shadow-xl">
+    <nav className="bg-navbar-grey text-white text-md font-bold p-5 flex justify-center space-x-6 shadow-2xl">
       <a href="#home" className="hover:text-slate-500 transition">Home</a>
       <a href="#about" className="hover:text-slate-500 transition">About</a>
       <a href="#projects" className="hover:text-slate-500 transition">Projects</a>
