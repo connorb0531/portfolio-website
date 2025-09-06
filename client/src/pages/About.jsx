3b136c21c89b97cb8e6f08c32e7db8c057c06160
Connor Buckley <connorbuckley144@gmail.com>
2025-09-06 01:17:06 -0700

    Update AboutPage: add portrait.gif

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,7 @@
 // About.jsx
 import SlideIn from "../components/SlideIn";
 import TechStack from "../components/TechStack";
+import HeadshotGif from "../components/HeadshotGif";
 
 export default function About() {
     return (
@@ -11,12 +12,7 @@ export default function About() {
                 {/* Portrait + Text Column (slide from left) */}
                 <SlideIn axis="x" offset={-80} className="flex flex-col gap-6 flex-1">
                     <div className="flex justify-center">
-                        <img
-                            src="/profile_template.png"
-                            alt="Portrait"
-                            className="w-24 sm:w-32 md:w-40 lg:w-48 object-contain rounded-2xl shadow-md bg-gray-600"
-                            loading="lazy"
-                        />
+                        <HeadshotGif/>
                     </div>
                     <p className="text-lg leading-relaxed text-left px-5">
                         I'm a computer science student at San José State University with an interest in physics, simulations, and data modeling.
