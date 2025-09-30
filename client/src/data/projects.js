a83be34aab0e501a48fa5a47273d981c2bffd8fa
Connor Buckley <connorbuckley144@gmail.com>
2025-09-30 08:46:27 -0700

    Add project: Mushroom CNN classifier

--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -1,5 +1,12 @@
 export const projects = [
     {
+        title: "CNN Mushrom Classifier",
+        blurb:
+            "Full-stack website for machine learning mushroom image classification with a Spring Boot + PostgreSQL backend and React/Vite frontend.",
+        image: "/projects/mushroom-vision.png",
+        link: "https://github.com/connorb0531/mushroom-vision/tree/pytorch",
+    },
+    {
         title: "NASA DE440 Simulator",
         blurb:
             "Solar system simulator inspired by NASA's JPL DE440 ephemerides. Built from scratch in Python using real-world data, Newtonian physics, and numerical integrators.",
@@ -12,13 +19,6 @@ export const projects = [
             "Website for a local construction business to showcasing previous services/projects. Full-stack site with MongoDB, Express, React, Node. Deployed on Render.",
         image: "/projects/construction-website1.png",
         link: "https://tbuckdev.com",
-    },
-    {
-        title: "FitBit Conntected Lock",
-        blurb:
-            "Solenoid lock using Fitbit API and Raspberry Pi to unlock a door when activity goals (steps, distance, calories) are tracked and achieved on FitBit watch.",
-        image: "/projects/fitbit-lock1.jpeg",
-        link: "https://github.com/connorb0531/FitBit-Lock",
     },
     {
         title: "Discord Chess Bot",
